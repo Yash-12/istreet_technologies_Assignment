fd8f7ab04345555909038e050a6fdf790271ce0c
Yash-12 <68053482+Yash-12@users.noreply.github.com>
2025-04-20 10:39:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Navbar />
         <div className="container mt-4">
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route
               path="/admin"
               element={
